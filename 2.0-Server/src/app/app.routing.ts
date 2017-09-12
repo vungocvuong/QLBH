2d88a9968667c6e62808d6e89bfb20cea35195d9
Vu Ngoc Vuong <vu.ngoc.vuong@mqsolutions.vn>
2017-09-12 14:46:01 +0700

    Chỉnh sửa nav bar inside

--- 2.0-Server/src/app/app.routing.ts
+++ 2.0-Server/src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -12,16 +12,25 @@ import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
 
-const routes: Routes =[
-    { path: 'dashboard',      component: DashboardComponent },
-    { path: 'user-profile',   component: UserProfileComponent },
-    { path: 'table-list',     component: TableListComponent },
-    { path: 'typography',     component: TypographyComponent },
-    { path: 'icons',          component: IconsComponent },
-    { path: 'maps',           component: MapsComponent },
-    { path: 'notifications',  component: NotificationsComponent },
-    { path: 'upgrade',        component: UpgradeComponent },
-      { path: '',          redirectTo: 'dashboard', pathMatch: 'full' }
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'product', component: UserProfileComponent },
+  { path: 'sale-invoice', component: TableListComponent },
+  { path: 'purchase-invoice', component: TypographyComponent },
+  { path: 'customer', component: IconsComponent },
+  { path: 'user', component: MapsComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+
+
+  //Test
+  { path: 'user-profile',   component: UserProfileComponent },
+  { path: 'table-list',     component: TableListComponent },
+  { path: 'typography',     component: TypographyComponent },
+  { path: 'icons',          component: IconsComponent },
+  { path: 'maps',           component: MapsComponent },
+  { path: 'notifications',  component: NotificationsComponent },
+  { path: 'upgrade',        component: UpgradeComponent },
+
 ];
 
 @NgModule({
