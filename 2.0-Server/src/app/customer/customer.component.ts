10f434133f0bdf9cd35ac3a376329cde60f2eb23
Vu Ngoc Vuong <vu.ngoc.vuong@mqsolutions.vn>
2017-09-13 09:53:11 +0700

    customer: add model object

--- 2.0-Server/src/app/customer/customer.component.ts
+++ 2.0-Server/src/app/customer/customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer, FacebookInfo } from './customer.model';
 
 @Component({
   selector: 'app-customer',
@@ -6,16 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent implements OnInit {
-  customers: any[];
-
+  customers: Array<Customer> = [];
   constructor() { }
 
   ngOnInit() {
-    this.customers = ["1", "2", "3", "4", "5"];
-
-    for (var i = 0; i < 3; i++) {
-      this.customers.push("a")
+    for (var i = 0; i < 5; i++) {
+      var cus = new Customer();
+      cus.id = i.toString();
+      cus.name = "Dang Thu Trang";
+      cus.address = "Số 72A Nguyễn Trãi, P. Thượng Đình, Q. Thanh Xuân, Tp. Hà Nội";
+      cus.phone = "0967245018";
+      cus.customerType = "Kim cuong";
+      cus.lastBought = Date.now();
+      cus.facebook = new FacebookInfo(
+        "Vu Ngoc Vuong",
+        "https://scontent.fhan3-1.fna.fbcdn.net/v/t1.0-1/p160x160/17759930_1880925222123674_8462300566271354203_n.jpg?oh=80aff32da05c057650758e4cae9e9a29&oe=5A14C074",
+        "1",
+        "1"
+       );
+      this.customers.push(cus);
     }
-
   }
 }
---
+++ 2.0-Server/src/app/customer/customer.model.ts
@@ -0,0 +1,20 @@
+
+export class Customer {
+    public id: string;
+    public name: string;
+    public address: string;
+    public phone: string;
+    public customerType: string;
+    public lastBought: number;
+    public facebook: FacebookInfo;
+}
+
+export class FacebookInfo {
+    constructor (
+        public name:string,
+        public avarta:string,
+        public id:string,
+        public messageId:string
+    ) {}
+
+}
